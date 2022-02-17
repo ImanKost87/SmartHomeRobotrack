ed450dd47105f02edc52456a3e39a950e7d43596
Konstantin <81853289+ImanKost87@users.noreply.github.com>
2022-02-17 12:45:46 +0300

    Update Control.ino

--- Control.ino
+++ Control.ino
@@ -1,4 +1,5 @@
 int lum = 0 ;
+int ir_lum = 0;
 String namedSensors = "";
 
 void setup()
@@ -6,7 +7,7 @@ void setup()
   Serial.begin(115200);
   Serial1.begin(115200);
   
-  String sensors = "4:IN1,21:OUT1";
+  String sensors = "4:IN1,21:OUT1,1:IN8";
   String messageFromWemos = "";
   int i = 0;
   while (messageFromWemos == "") {
@@ -23,18 +24,40 @@ void setup()
 
 void loop()
 {
-  delay(500);
+  delay(2000);
   
-  lum = cdsAnalog(IN1) ;
-  if (Serial1.available()) {
-    Serial1.println("E1:" + String(lum));
-    Serial.println("E1:" + String(lum));
+  lum = cdsAnalog(IN1);
+  ir_lum = irAnalog(IN8);
+  
+  Serial1.println("E1:" + String(lum) + ",B1:" + String(ir_lum));
+  Serial.println("E1:" + String(lum) + ",B1:" + String(ir_lum));
+  
+  String str = Serial1.readString();
+  Serial.println(str);
+  char data[str.length()];
+  str.toCharArray(data, str.length());
+  char *rest = NULL;
+  char *token;
+
+  for (token = strtok_r(data, ",", &rest);
+       token != NULL;
+       token = strtok_r(NULL, ",", &rest)) {
+    char *subrest = NULL;
+    char *subtoken; int i;
+    for (subtoken = strtok_r(token, ":", &subrest), i = 0;
+         subtoken != NULL;
+         subtoken = strtok_r(NULL, ":", &subrest), i++) {
+      if (i == 1 || i == 0) {
+        String out = String(str);
+        if (out.indexOf("T") != -1) {
+          led(OUT1, HIGH);
+          delay(1000);
+        } else if (out.indexOf("F") != -1){
+          led(OUT1, LOW);
+        }
+      }
+    }
   }
-  if (lum > 500) {
-    led(OUT1, HIGH);
-  } else {
-    led(OUT1, LOW);
-  } 
 }
 
 void registerSensors(String str) {
@@ -67,4 +90,3 @@ void registerSensors(String str) {
   Serial1.print(namedSensors);
   Serial.println(namedSensors);
 }
-
